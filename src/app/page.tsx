083c6cf7815cc253868236c71a28976338aa37f4
Cleo <121153199+cleoreyes@users.noreply.github.com>
2025-05-27 18:24:37 -0700

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,54 +2,102 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="min-h-screen flex items-center mx-auto w-3/4">
+      <main className="flex flex-col items-center sm:items-start">
         <div className="flex flex-col sm:flex-row items-center sm:items-start gap-8">
-          <div>
+          <div className="w-full">
             <Image
               src="https://media.licdn.com/dms/image/v2/D5603AQFLCUblM1vkKQ/profile-displayphoto-shrink_400_400/B56ZYzW9fYHoAo-/0/1744618379910?e=1753920000&v=beta&t=R8V6opQTy6Z6aW3q3bjP7T2gbfT5hvjEecGpWIKSDJI"
               alt="Cleo Reyes"
-              width={800}
-              height={800}
-              className="rounded-sm"
+              width={1500}
+              height={2000}
+              className="rounded-sm w-full h-auto object-cover aspect-[4/5] max-w-none"
             />
             <h1>Contact Info</h1>
             <p>creyes25@uw.edu</p>
-              <a
-                href="https://www.linkedin.com/in/cleoreyes/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LinkedIn
-              </a>
+            <a
+              href="https://www.linkedin.com/in/cleoreyes/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
           </div>
           <div className="flex flex-col items-center sm:items-start gap-10">
             <div className="flex flex-col sm:flex-row items-center sm:items-start gap-8">
               <div className="flex flex-col gap-2">
                 <h1 className="text-3xl">Hey there, my name is Cleo Reyes</h1>
                 <p>
-                  I&apos;m an undergraduate at the Univeristy of Washington
-                  studying Computer Science and Informatics. My interests lie in
-                  software engineering, cybersecurity, human-computer
-                  interaction, networks, and distributed systems. I am pursuing
-                  a career in software engineering and I am passionate about
-                  building software that makes a difference in people&apos;s
-                  lives. I have experience in software engineering, web
-                  development, and data science, UI/UX design and I am always
-                  eager to learn new technologies and improve my skills.
+                  I&apos;m an undergraduate at the{" "}
+                  <a
+                    href="https://www.washington.edu/"
+                    className="italic hover:underline text-gray-800"
+                  >
+                    Univeristy of Washington{" "}
+                  </a>
+                  studying{" "}
+                  <a
+                    className="italic hover:underline text-gray-800"
+                    href="https://www.cs.washington.edu/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Computer Science
+                  </a>{" "}
+                  and{" "}
+                  <a
+                    className="italic hover:underline text-gray-800"
+                    href="https://www.ischool.uw.edu"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Informatics
+                  </a>
+                  . My interests lie in software engineering, cybersecurity,
+                  human-computer interaction, networks, and distributed systems.
+                  I am pursuing a career in software engineering and I am
+                  passionate about building software that makes a difference in
+                  people&apos;s lives. I have experience in software
+                  engineering, web development, and data science, UI/UX design
+                  and I am always eager to learn new technologies and improve my
+                  skills.
                 </p>
                 <p>
-                  I was previously a Software Engineer Intern at the Allen
-                  Institute where I worked in the Application development team,
+                  I was previously a Software Engineer Intern at the{" "}
+                  <a
+                    className="italic hover:underline text-gray-800"
+                    href="https://alleninstitute.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Allen Institute
+                  </a>{" "}
+                  where I worked in the Application development team,
                   spearheading the implmentation and improvement of
                   accessibility for web applications for scientists and
                   researchers. I also worked on the development of a features
                   that allows scientists to visualize and analyze large
                   datasets. Additionally, I was a an Undergraduate Research
-                  Assistant at the University of Washington Code and Cognition
-                  Lab led by Dr. Amy J. Ko, where I worked on developint
-                  &quot;WordPlay&quot;, an accessible and inclusive programming
-                  language platform for all backgrounds.
+                  Assistant at the University of Washington{" "}
+                  <a
+                    href="https://faculty.washington.edu/ajko/lab"
+                    className="italic hover:underline text-gray-800"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Code and Cognition Lab led by Dr. Amy J. Ko
+                  </a>
+                  , where I worked on developing{" "}
+                  <a
+                    href="https://wordplay.dev/"
+                    className="italic hover:underline text-gray-800"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    &quot;WordPlay&quot;
+                  </a>
+                  , an accessible and inclusive programming language platform
+                  for all backgrounds.
                 </p>
                 <p>
                   I will be joining Amazon Web Services as a Software
